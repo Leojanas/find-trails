aad0ea97d98a7971eb5ca6e8ec883bb4fbb91dfc
Leo Janas <leojanas7@gmail.com>
2020-08-18 20:04:19 -0600

    Add Geocoding API fetch functionality

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 //This is the javascript to run the find trails app
 //Written by Leo Janas
+"use strict";
 
-
+let searchLocation = '';
+let lat = 0;
+let long = 0;
+let loc = '';
 
 //function to render the error screen for invalid search
 
 //function to render the main page on a 'New Search' click
 function renderMain(){
+    $('#main-form').off('click', '#submit', clickSearch)
+$('#main-form').off('click', '#new-search', clickNewSearch)
     $('#main-form').html(
         `<div class="group">
-        <button class="item" id="address">Full Address</button>
-        <button class="item" id="city">City</button>
-        <button class="item" id="zip">Zip Code</button>
+        <button class="item" id="select-address">Full Address</button>
+        <button class="item" id="select-city">City</button>
+        <button class="item" id="select-zip">Zip Code</button>
     </div>
     <button id="info">More Info</button>`
     )
@@ -24,12 +30,38 @@ function renderMain(){
 //function to access the hiking project API to retrieve results
 
 //function to generate the query for the hiking access API based on search terms and coordinates
+function hikingQuery(lat,long){
+    console.log(lat);
+    console.log(long);
+}
 
 //function to access the google geocoding API to retrieve coordinates for the search
+function fetchGeocoding(location){
+    let string = '';
+    const mapsBaseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?';
+    if (location === 'address'){
+        let string = $('#address').val() +' '+ $('#city').val() +' '+ $('#state').val() +' '+ $('#zip').val();
+        searchLocation = encodeURIComponent(string);
+    }else  if (location === 'city'){
+        let string = $('#city').val() +' '+ $('#state').val();
+        searchLocation = encodeURIComponent(string);
+    }else  if (location === 'zip'){
+        let string = $('#zip').val();
+        searchLocation = encodeURIComponent(string);
+    }
+    const mapsURL = mapsBaseUrl + 'address=' + searchLocation + '&key=AIzaSyCvS2cO3sDJcmoHQn0h46ITWIlhczMMsU4';
+    fetch(mapsURL)
+    .then(response => response.json())
+    .then(responseJson => {
+        lat = responseJson.results[0].geometry.location.lat;
+        long= responseJson.results[0].geometry.location.lng;
+        hikingQuery(lat,long);
+    });
+    
+}
 
 //function to render the search screen based on the type of search chosen
 function renderSearch(location){
-    watchSearch();
     if (location === 'address'){
         $('#main-form').html(`
             <div>
@@ -52,8 +84,7 @@ function renderSearch(location){
             <input type="text" id="min-length">
             </div>
             <button id="new-search">New Search</button>
-            <button id="submit">Search</button>
-            `
+            <button id="submit">Search</button>`
         )
     }else if(location === 'city'){
         $('#main-form').html(`
@@ -72,8 +103,7 @@ function renderSearch(location){
             <input type="text" id="min-length">
             </div>
             <button id="new-search">New Search</button>
-            <button id="submit">Search</button>
-            `
+            <button id="submit">Search</button>`
         )
     }else if(location === 'zip'){
         $('#main-form').html(`
@@ -90,55 +120,53 @@ function renderSearch(location){
             <input type="text" id="min-length">
             </div>
             <button id="new-search">New Search</button>
-            <button id="submit">Search</button>
-            `
+            <button id="submit">Search</button>`
         )
     }
-
-
+    loc = location;
+    watchSearch();
 }
 
 //function to render the detailed info screen
 function renderInfo(){
 
 }
-//watchSearch function to see when a full search is submitted
-function watchSearch(){
-$('#main-form').on('click', '#submit', function(event){
+//function for what to do on search submission
+function clickSearch(){
     event.preventDefault();
-    console.log('Search submitted');
-})
-$('#main-form').on('click', '#new-search', function(event){
+    fetchGeocoding(loc);
+}
+//function for what to do on new search
+function clickNewSearch(){
     event.preventDefault();
     renderMain();
-    console.log('New search clicked');
-})
+}
+//watchSearch function to listen for search submissions
+function watchSearch(){
+$('#main-form').on('click', '#submit', clickSearch)
+$('#main-form').on('click', '#new-search', clickNewSearch)
 }
 
 //watchMain function to see which button is clicked on the main screen
 function watchMain(){
-$('#main-form').on('click','#address', function(event) {
+$('#main-form').on('click','#select-address', function(event) {
     event.preventDefault();
-    let location = 'address'
+    const location = 'address';
     renderSearch(location);
-    console.log('Address selected');
 })
-$('#main-form').on('click','#city', function(event){
+$('#main-form').on('click','#select-city', function(event){
     event.preventDefault();
-    let location = 'city'
+    const location = 'city';
     renderSearch(location);
-    console.log('City selected');
 })
-$('#main-form').on('click','#zip', function(event){
+$('#main-form').on('click','#select-zip', function(event){
     event.preventDefault();
-    let location = 'zip'
+    const location = 'zip';
     renderSearch(location);
-    console.log('Zip selected');
 })
 $('#main-form').on('click','#info', function(event){
     event.preventDefault();
     renderInfo();
-    console.log('Info selected');
 })
 }
 
